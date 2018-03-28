b839e17d1595b8e0be2cc60677b264f913c602a9
Alkud <alexandr_kudinov@mail.ru>
2018-03-28 13:25:45 +0300

    improve test

--- otus_hw_3_test.cpp
+++ otus_hw_3_test.cpp
@@ -8,33 +8,48 @@
 #include <cstdlib>
 #include <iostream>
 
+uint factorial(uint n)
+{
+  uint result{1};
+  for (uint i{2}; i <= n; i++)
+    result *= i;
+  return result;
+}
 
 BOOST_AUTO_TEST_SUITE(homework_3_test)
 
 BOOST_AUTO_TEST_CASE(map_test_insert)
 {
+  custom_map<int, int> map3{};
+  for (int idx{}; idx < 10; idx++)
+    map3.insert(std::pair<const int, int>{idx, factorial(idx)});
+
   custom_map<int, int> testMap{};
-  testMap.insert(std::make_pair<int, int>(2, 2));
-  testMap.insert(std::make_pair<int, int>(1, 1));
-  testMap.insert(std::make_pair<int, int>(3, 3));
+  testMap.insert(0, 0);
+  testMap.insert(1, 1);
+  testMap.insert(2, 2);
   size_t presentNodesCount{};
   for(auto item:testMap)
+  {
     presentNodesCount++;
+  }
   BOOST_CHECK(presentNodesCount == testMap.count() &&
               presentNodesCount == 3);
 }
 
 BOOST_AUTO_TEST_CASE(map_test_sorting)
 {
-  custom_map<std::string, std::string> testMap{};
+  custom_map<std::string, std::string>testMap{};
   testMap.insert(std::string{"asDFGH d"}, std::string{"asthdfgs564 64"});
-  testMap.insert(std::string{"atDFGH d"}, std::string{"asthdfgss564 64"});
+  testMap.insert(std::string{"asFFGH d"}, std::string{"asthdfgss564 64"});
   testMap.insert(std::string{"bsDFGH d"}, std::string{"asdfgthgs564 64"});
-  testMap.insert(std::string{"bsCFGH d"}, std::string{"assdfgsdhgs564 64"});
+  testMap.insert(std::string{"bsFFGH d"}, std::string{"assdfgthgs564 64"});
+  testMap.insert(std::string{"bsjFGH d"}, std::string{"asssdfgthgs564 64"});
 
   size_t wrongPlacementsCount{};
-  std::string previous{(testMap.begin())->first};
-  for (auto iter{++testMap.begin()}; iter != testMap.end(); iter++)
+  auto mapBegin{testMap.begin()};
+  std::string previous{mapBegin->first};
+  for (auto iter{std::next(testMap.begin())}; iter != testMap.end(); iter++)
   {
     if (iter->first < previous)
       wrongPlacementsCount++;
